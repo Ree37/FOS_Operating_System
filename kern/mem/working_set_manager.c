d85c28b4aa2285168ed2eb75cd1f78c6dda98748
Ree37 <rimo.mrben@gmail.com>
2024-11-19 18:03:19 +0200

    env_page_ws_list_create_element done

--- kern/mem/working_set_manager.c
+++ kern/mem/working_set_manager.c
@@ -19,8 +19,21 @@ inline struct WorkingSetElement* env_page_ws_list_create_element(struct Env* e, 
 	//TODO: [PROJECT'24.MS2 - #07] [2] FAULT HANDLER I - Create a new WS element
 	//If failed to create a new one, kernel should panic()!
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("env_page_ws_list_create_element is not implemented yet");
+	//panic("env_page_ws_list_create_element is not implemented yet");
 	//Your Code is Here...
+	if(LIST_SIZE(&e->page_WS_list) >= e->page_WS_max_size){
+		panic("Not enough space in WS List");
+	}
+	struct WorkingSetElement* w = kmalloc(sizeof(struct WorkingSetElement));
+	if(w == NULL){
+		panic("working set panic");
+	}
+	w->virtual_address = virtual_address;
+	LIST_INSERT_TAIL(&e->page_WS_list,w);
+	if(LIST_SIZE(&e->page_WS_list) == e->page_WS_max_size){
+		e->page_last_WS_element = w;
+	}
+	return w;
 
 }
 inline void env_page_ws_invalidate(struct Env* e, uint32 virtual_address)
